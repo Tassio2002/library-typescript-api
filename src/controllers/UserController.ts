998adcee9b933eafb5d55ebee8e20ad30cb5fbff
Tassio <81255475+Tassio2002@users.noreply.github.com>
2023-02-16 12:34:26 -0300

    create new user route and controller

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { userRepository } from "../repositories/userRepository";
+
+export class UserController {
+  async signup(req: Request, res: Response) {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      res.status(400).send({
+        "error message": "All requested information is required.",
+      });
+    }
+
+    try {
+      const newUser = userRepository.create({
+        name,
+        email,
+        password,
+      });
+
+      await userRepository.save(newUser);
+
+      return res.status(201).json(newUser);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        "error message": "Internal Server Error",
+      });
+    }
+  }
+}
