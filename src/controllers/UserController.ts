6f7094efd22e68de59658d019299e0a472071219
Tassio <81255475+Tassio2002@users.noreply.github.com>
2023-02-24 19:29:28 -0300

    Review changes

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -9,15 +9,14 @@ export class UserController {
   async signup(req: Request, res: Response) {
     const { name, email, password } = req.body;
 
-    const userExists = await userRepository.findOneBy({ email });
-
-    if (userExists) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-
-    const passwordHash = await encrypt(password);
-
     try {
+      const userExists = await userRepository.findOneBy({ email });
+
+      if (userExists) {
+        return res.status(400).json({ message: "User already exists" });
+      }
+
+      const passwordHash = await encrypt(password);
       const newUser = userRepository.create({
         name,
         email,
@@ -38,29 +37,36 @@ export class UserController {
 
   async login(req: Request, res: Response) {
     const { email, password } = req.body;
+    try {
+      const expirationTimeInSeconds = 300;
+      const user = await userRepository.findOneBy({ email });
 
-    const user = await userRepository.findOneBy({ email });
+      if (!user) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
 
-    if (!user) {
-      return res.status(401).json({ message: "Invalid email or password" });
+      const verifyPassword = await decryptPassword(password, user.password);
+
+      if (!verifyPassword) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+
+      const token = jwt.sign({ id: user.id }, process.env.JWT_PASS ?? "", {
+        expiresIn: expirationTimeInSeconds,
+      });
+
+      const { password: _, ...userLogin } = user;
+
+      return res.status(200).json({
+        user: userLogin,
+        token: token,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send({
+        "error message": "Internal Server Error",
+      });
     }
-
-    const verifyPassword = await decryptPassword(password, user.password);
-
-    if (!verifyPassword) {
-      return res.status(401).json({ message: "Invalid email or password" });
-    }
-
-    const token = jwt.sign({ id: user.id }, process.env.JWT_PASS ?? "", {
-      expiresIn: 300,
-    });
-
-    const { password: _, ...userLogin } = user;
-
-    return res.status(200).json({
-      user: userLogin,
-      token: token,
-    });
   }
 
   async createBook(req: Request, res: Response) {
--- src/middlewares/authLogin.ts
+++ src/middlewares/authLogin.ts
@@ -12,24 +12,27 @@ export const authMiddleware = async (
   next: NextFunction
 ) => {
   const { authorization } = req.headers;
+  try {
+    if (!authorization) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
 
-  if (!authorization) {
+    const token = authorization.split(" ")[1];
+
+    const { id } = jwt.verify(token, process.env.JWT_PASS ?? "") as JwtPayload;
+
+    const user = await userRepository.findOneBy({ id });
+
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const { password: _, ...loggedUser } = user;
+
+    req.user = loggedUser;
+
+    next();
+  } catch (error) {
     return res.status(401).json({ message: "Unauthorized" });
   }
-
-  const token = authorization.split(" ")[1];
-
-  const { id } = jwt.verify(token, process.env.JWT_PASS ?? "") as JwtPayload;
-
-  const user = await userRepository.findOneBy({ id });
-
-  if (!user) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
-  const { password: _, ...loggedUser } = user;
-
-  req.user = loggedUser;
-
-  next();
 };
