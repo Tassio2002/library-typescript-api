0a32a7fd780511cc3ce818842d5adbb8d8cdab70
Tassio <81255475+Tassio2002@users.noreply.github.com>
2023-03-02 09:46:43 -0300

    Error 404 added

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { BookController } from "../controllers/BookController";
+import { ErrorController } from "../controllers/ErrorController";
 import { UserController } from "../controllers/UserController";
 import { authMiddleware } from "../middlewares/authLogin";
 import { schemaValidation } from "../middlewares/schemaValidation";
@@ -8,17 +9,25 @@ import { LoginSchema } from "../schemas/LoginSchema";
 import { UserSchema } from "../schemas/UserSchema";
 
 const router = Router();
-const userController = new UserController()
-const bookController = new BookController()
+const userController = new UserController();
+const bookController = new BookController();
+const errorController = new ErrorController();
 
 router.post("/signup", schemaValidation(UserSchema), userController.signup);
 router.post("/login", schemaValidation(LoginSchema), userController.login);
-router.post("/create_reserve/:user_logged/:book_id", bookController.createReserve);
+router.post(
+  "/create_reserve/:user_logged/:book_id",
+  bookController.createReserve
+);
 
+router.use(authMiddleware);
 
-router.use(authMiddleware)
+router.post(
+  "/book/:user_id/create",
+  schemaValidation(BookSchema),
+  userController.createBook
+);
+router.get("/book/all_books", bookController.list);
 
-router.post("/book/:user_id/create", schemaValidation(BookSchema) ,userController.createBook)
-router.get("/book/all_books", bookController.list)
-
+router.get("*", errorController.error404);
 export default router;
